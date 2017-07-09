4f09a0d16b0b6fefc68a4c7e5866189971ae3ac2
feixia5320 <1044489176@qq.com>
2017-07-09 22:28:14 +0800

    建立框架

---
+++ js/main.js
@@ -0,0 +1,39 @@
+/*
+获取顶层对象
+*/
+
+// 方法一
+(typeof window !== 'undefined'
+   ? window
+   : (typeof process === 'object' &&
+      typeof require === 'function' &&
+      typeof global === 'object')
+     ? global
+     : this);
+
+// 方法二
+var getGlobal = function () {
+  if (typeof self !== 'undefined') { return self; }
+  if (typeof window !== 'undefined') { return window; }
+  if (typeof global !== 'undefined') { return global; }
+  throw new Error('unable to locate global object');
+};
+var glo = getGlobal();
+
+/*
+let [x, , y] = [1, 2, 3];
+console.log(x);
+console.log(y);
+*/
+
+function* fibs() {
+  let a = 0;
+  let b = 1;
+  while (true) {
+    yield a;
+    [a, b] = [b, a + b];
+  }
+}
+
+let [first, second, third, fourth, fifth, sixth] = fibs();
+console.log(sixth);
